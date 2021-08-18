e76b87057cce0ab7239d430005af2ba864429c24
mlodykanesa <jadziablet@gmail.com>
2021-08-18 14:25:05 +0200

    c

--- CryptoMiner/ViewController.swift
+++ CryptoMiner/ViewController.swift
@@ -16,11 +16,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var MINERS: UIButton!
     @IBOutlet weak var SELL_KC: UIButton!
     @IBOutlet weak var SHOP: UIButton!
+    @IBOutlet weak var pomoctxt: UILabel!
+    @IBOutlet weak var rewardtxt: UILabel!
+    
     let userDefaults = UserDefaults()
     
     // GLOBAL VARIABLES
     struct Zmienne {
-        static var Z_KC_CENA = 1000
+        static var Z_KC_CENA = Int.random(in: 300..<2000)
         static var Z_KC_OWN: Double = 0
         static var Z_KC_HR: Double = 0
         static var Z_WALLET = 2000
@@ -32,6 +35,14 @@ class ViewController: UIViewController {
         static var MINEPRO2000PWR = 0.4
         static var CENA_Z220 = 200
         static var CENA_Z220T = 500
+        static var timestamp = Date().timeIntervalSince1970
+        static var delay = 0
+        static var startDate = Date()
+        static var CurrentDate: Date = Date()
+        static var Difference = ""
+        static var Reward: Date = Date()
+        static var Jebne = ""
+        
         
         
     
@@ -40,6 +51,8 @@ class ViewController: UIViewController {
     var TimerGodz: Timer!
     var TimerKurs: Timer!
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         KC_CENA.text = String(Zmienne.Z_KC_CENA)
         KC_OWN.text = String(Zmienne.Z_KC_OWN)
@@ -53,11 +66,10 @@ class ViewController: UIViewController {
         KC_HR.text = String(Zmienne.Z_KC_HR)
         // timery
         Timer2 = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(refresh), userInfo: nil, repeats: true)
-       
-        TimerGodz = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(refresh2), userInfo: nil, repeats: true)
         TimerKurs = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(refreshKurs), userInfo: nil, repeats: true)
         //timery\>
         load()
+        userDefaults.setValue(Zmienne.startDate, forKey: "startDate_")
         
         
     }
@@ -71,17 +83,31 @@ class ViewController: UIViewController {
         Zmienne.Z_KC_HR = wynik
         
     }
+    func liczenieDelayP(){
+        let formatter = DateComponentsFormatter()
+           formatter.allowedUnits = [.second]
+        formatter.unitsStyle = .positional
+            Zmienne.Difference = formatter.string(from: Zmienne.startDate, to: Zmienne.CurrentDate)!
+        pomoctxt.text = Zmienne.Difference
+    }
+    
+    
     func save(){
         userDefaults.setValue(Zmienne.Z_WALLET, forKey: "kasa")
         userDefaults.setValue(Zmienne.Z_KC_OWN, forKey: "kc_own")
         userDefaults.setValue(Zmienne.Z_KC_HR, forKey: "kc_hr")
         userDefaults.setValue(Zmienne.KUPIONE_ZTX220, forKey: "k_z220")
         userDefaults.setValue(Zmienne.KUPIONE_ZTX220T, forKey: "k_z220t")
+        userDefaults.setValue(Zmienne.CENA_Z220, forKey: "cena_z220")
+        userDefaults.setValue(Zmienne.CENA_Z220T, forKey: "cena_z220t")
+        userDefaults.setValue(Zmienne.timestamp, forKey: "tsp")
+        userDefaults.setValue(Zmienne.Difference, forKey:"diff")
+        
         
         
         
     }
-    func load(){
+     func load(){
         if let kasa = userDefaults.value(forKey: "kasa") as? Int {
             Zmienne.Z_WALLET = kasa }
         if let kc_own = userDefaults.value(forKey: "kc_own") as? Double {
@@ -97,14 +123,27 @@ class ViewController: UIViewController {
             Zmienne.KUPIONE_ZTX220T = k_z220t
         }
         
+        if let delay = userDefaults.value(forKey: "tsp") as? Int{
+            Zmienne.timestamp = TimeInterval(delay)
+            
+        }
+        if let startDate_ = userDefaults.value(forKey: "startDate_") as? Date{
+            Zmienne.startDate = startDate_
+        }
+        if let cena220 = userDefaults.value(forKey: "cena_z220") as? Int{
+            Zmienne.CENA_Z220 = cena220
+        }
+        if let cena220t = userDefaults.value(forKey: "cena_z220t") as? Int{
+            Zmienne.CENA_Z220T = cena220t
+        }
+        
+        
+        
         
     }
     
     
     
-    @objc func refresh2(){
-        Zmienne.Z_KC_OWN = (Zmienne.Z_KC_OWN + (Zmienne.Z_KC_HR)/6)
-    }
     @objc func refreshKurs(){
         let x = Int.random(in: 300..<2000)
         Zmienne.Z_KC_CENA = x
@@ -113,11 +152,15 @@ class ViewController: UIViewController {
     @objc func refresh(){
         liczenie()
         save()
-        load()
+        Zmienne.CurrentDate = Date()
+        liczenieDelayP()
+        Zmienne.Z_KC_OWN = (Zmienne.Z_KC_OWN + (Zmienne.Z_KC_HR)/60)
         KC_CENA.text = String(Zmienne.Z_KC_CENA)
         KC_OWN.text = String(format: "%.2f", Zmienne.Z_KC_OWN)
         KC_HR.text = String(format: "%.2f", Zmienne.Z_KC_HR)
         WALLET.text = String(Zmienne.Z_WALLET)
+        
+        
         
         
     }
@@ -153,6 +196,11 @@ class ViewController: UIViewController {
         Zmienne.Z_KC_OWN = 0
         Zmienne.KUPIONE_ZTX220 = 0
         Zmienne.KUPIONE_ZTX220T = 0
+        Zmienne.CENA_Z220 = 200
+        Zmienne.CENA_Z220T = 500
+        Zmienne.startDate = Date()
+        userDefaults.setValue(Zmienne.startDate, forKey: "startDate_")
+        
         
     }
     
